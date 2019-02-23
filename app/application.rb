ecc6e60d25289a3fee5ac777e8535591ba92d9bb
Sara Walker <sarawall1187@gmail.com>
2019-02-23 18:42:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 class Application 
-  @@items = [Item.new("Pears", 0.99]
+  @@items = [Item.new("Figs",3.42), Item.new("Pears", 0.99)]
   def call 
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
