193c490a708b2066b48b971c3c44db66babc22af
Sara Walker <sarawall1187@gmail.com>
2019-02-23 18:50:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
    
       if req.path=="/items"
-       binding.pry  
+       
       resp.write item_check
     else
       resp.write "Route not found"
