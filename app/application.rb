4bbaa8c685ca8980f0df2cba8d645b507938ed1e
Sara Walker <sarawall1187@gmail.com>
2019-02-23 18:43:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
       if req.path=="/items"
-      # resp.write "You requested the songs"
+      resp.write item_check
     else
       resp.write "Route not found"
       resp.status = 404
